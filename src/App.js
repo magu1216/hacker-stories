4a0db3618cfd945c4434ae16d4dd3ad370dd8936
Markus Guertner <guertnermarkus@gmail.com>
2020-11-01 18:06:02 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const welcome = {
+  prefix: "Hello",
+  name: "Markus",
+};
+
+function getTitle(name) {
+  return name;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        {welcome.prefix} {getTitle(welcome.name)}
+      </h1>
+
+      <label htmlFor="search">Search: </label>
+      <input type="text" name="The Input" id="search" />
     </div>
   );
 }
