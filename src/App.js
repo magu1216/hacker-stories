168a3654818cc4b40e0d978c9374d67abbf00de4
Markus Guertner <guertnermarkus@gmail.com>
2020-11-01 19:44:48 +0100

    lists and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
+import List from "./List";
 
 const welcome = {
   prefix: "Hello",
   name: "Markus",
 };
+
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function getTitle(name) {
   return name;
@@ -16,7 +36,8 @@ function App() {
       <h1>
         {welcome.prefix} {getTitle(welcome.name)}
       </h1>
-
+      <hr />
+      <List />
       <label htmlFor="search">Search: </label>
       <input type="text" name="The Input" id="search" />
     </div>
